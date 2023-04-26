0e85001cb7054bfcf4b12794da44f29958f7ec4e
Yavuz Kopar <kopar.yavuz@gmail.com>
2023-04-26 15:16:23 +0300

    distorber

--- Assets/Scripts/Behaviour/VIPGuardBehaviour/GuardBehaviour.cs
+++ Assets/Scripts/Behaviour/VIPGuardBehaviour/GuardBehaviour.cs
@@ -5,8 +5,26 @@ using UnityEngine.AI;
 
 public class GuardBehaviour : NPCBehaviour
 {
+    [SerializeField] bool isDistorbed;
+    float timer;
     private void Start()
     {
         animator.SetFloat("cycleOffset", Random.Range(0, 1f));
+    }
+    public void SetIsDistorbed(bool v)
+    {
+        isDistorbed = v;
+    }
+    private void Update() {
+        //Will be deleted
+        if (isDistorbed)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 3f)
+            {
+                isDistorbed = false;
+                timer = 0;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Yavuz/Otomat.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Otomat : MonoBehaviour,IDistorber,InteractableObjectsInterface
+{
+    [SerializeField] float distorbRange;
+    bool usedOnce;
+    public void Distorb()
+    {
+        // Distorb all Guards with in range
+        Collider[] guards = Physics.OverlapSphere(transform.position,distorbRange);
+        foreach (Collider guard in guards)
+        {
+            if (guard.TryGetComponent(out GuardBehaviour guardBehaviour))
+            {
+                guardBehaviour.SetIsDistorbed(true);
+            }
+        }
+    }
+
+    public void NotifyInteractableObjects()
+    {
+        if(!usedOnce)
+        {
+            Distorb();
+            usedOnce = true;
+        }
+        
+    }
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,distorbRange);
+    }
+}
